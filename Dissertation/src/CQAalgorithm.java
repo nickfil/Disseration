8d077fbcf56868e160ba2090e3b090f4bf9e4648
nickfil <nikolisfilip@gmail.com>
2020-02-10 22:51:19 +0000

    Removed Markov Chain class, finished main code

--- Dissertation/src/CQAalgorithm.java
+++ Dissertation/src/CQAalgorithm.java
@@ -2,7 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.Random; 
 
 import org.json.*;
 
@@ -19,11 +24,35 @@ public class CQAalgorithm {
 		SQLHandler database = new SQLHandler(username, password, "disstester"); //initializing our databse object
 		
 		ResultSet rs = database.query("SELECT * FROM main;");
-		Set<String> queryResults = database.getQueryResults(rs);
-		System.out.println(queryResults);
+		Set<String> databaseInstance = database.getQueryResults(rs);
+		//System.out.println(databaseInstance);
 		
-		//MarkovChain mChain = new MarkovChain();
+		double lamda = 0.1;
+		double epsilon = 0.1;
+		double n = (1/(2*Math.pow(epsilon, 2.0))) * Math.log(2/lamda);
 		
+		ArrayList<String> constraintViolating = new ArrayList<String>();
+		String tupleToRemove = null;
+		Random rand = new Random();
+		int counter = 0;
+		
+		for(int i=0; i<n; i++) {
+			Set<String> currentInstance = new HashSet<String>(databaseInstance);
+			
+			//while the instance is inconsistent
+			while(isInconsistent(currentInstance)) {
+				//get an ArrayList<String> of all inconsistent tuples
+				constraintViolating = violatingConstraintTuples(currentInstance);
+				
+				//choose one of the violations with prop 1/constraintViolating.length
+				tupleToRemove = constraintViolating.get(rand.nextInt(constraintViolating.size()));
+				//update databaseInstance
+				currentInstance.remove(tupleToRemove);
+			}
+			if(currentInstance.contains("(a,b)")) counter++;
+		
+		}
+		System.out.println(Double.valueOf(counter)/n);
 	}
 
 	public static void auth() throws JSONException, IOException {
@@ -38,4 +67,46 @@ public class CQAalgorithm {
         
 	}
 
+	public static ArrayList<String> violatingConstraintTuples(Set<String> db){
+
+		ArrayList<String> violations = new ArrayList<String>();
+		Map<Character, Integer> keys = new HashMap<Character, Integer>();
+		char primaryKey;
+		
+		for(String tuple : db) {
+			primaryKey = tuple.charAt(1);
+			if(keys.containsKey(primaryKey)) {
+				int count = keys.get(primaryKey);
+				count++;
+				keys.put(primaryKey, count);
+			}
+			else {
+				keys.put(primaryKey, 1);
+			}
+		}
+		
+		for(String tuple : db) {
+			primaryKey = tuple.charAt(1);
+			if(keys.get(primaryKey)>1) {
+				violations.add(tuple);
+			}
+		}
+		
+		return violations;
+	}
+	
+	public static Boolean isInconsistent(Set<String> db) {
+		ArrayList<Character> keys = new ArrayList<Character>();
+		
+		for(String each : db) {
+			if(keys.contains(each.charAt(1))) {
+				return true;
+			}
+			else {
+				keys.add(each.charAt(1));
+			}
+		}
+		
+		return false;
+	}
 }
--- Dissertation/src/SQLHandler.java
+++ Dissertation/src/SQLHandler.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javafx.util.Pair;
-
+//sudo pkill -u postgres
 public class SQLHandler {
 	
 	private static String username;
