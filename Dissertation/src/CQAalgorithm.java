ebf2bf35c2fe8a4e36c87e58d6809d0f2822e412
nickfil <nikolisfilip@gmail.com>
2020-02-20 22:19:46 +0000

    changes and completion of code

--- Dissertation/src/CQAalgorithm.java
+++ Dissertation/src/CQAalgorithm.java
@@ -3,51 +3,132 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.Random; 
+import java.util.Random;
 
 import org.json.*;
 
 public class CQAalgorithm {
 	private static String username;
 	private static String password;
+	private static final String q_tcc = "SELECT * FROM locations as l, crashes as c WHERE l.street_name=c.street_name AND l.street_no=c.street_no AND l.street_direction=c.street_direction";
+	private static final String q1_1 = "SELECT * FROM public_experiment_q1_1_10_2_5.lineitem as lineitem, public_experiment_q1_1_10_2_5.partsupp as partsupp WHERE lineitem.l_suppkey = partsupp.ps_suppkey";
+	private static final String q1_2 = "SELECT * FROM public_experiment_q1_1_30_2_5.lineitem as lineitem, public_experiment_q1_1_30_2_5.partsupp as partsupp WHERE lineitem.l_suppkey = partsupp.ps_suppkey";
+	private static final String q1_3 = "SELECT * FROM public_experiment_q1_1_50_2_5.lineitem as lineitem, public_experiment_q1_1_50_2_5.partsupp as partsupp WHERE lineitem.l_suppkey = partsupp.ps_suppkey";
+	private static final String q2_1 = "SELECT * FROM public_experiment_q2_1_10_2_5.lineitem as lineitem, public_experiment_q2_1_10_2_5.partsupp as partsupp, public_experiment_q2_1_10_2_5.supplier as supplier WHERE lineitem.l_suppkey = partsupp.ps_suppkey AND partsupp.ps_suppkey = supplier.s_suppkey";
+	private static final String q2_2 = "SELECT * FROM public_experiment_q2_1_30_2_5.lineitem as lineitem, public_experiment_q2_1_30_2_5.partsupp as partsupp, public_experiment_q2_1_30_2_5.supplier as supplier WHERE lineitem.l_suppkey = partsupp.ps_suppkey AND partsupp.ps_suppkey = supplier.s_suppkey";
+	private static final String q2_3 = "SELECT * FROM public_experiment_q2_1_50_2_5.lineitem as lineitem, public_experiment_q2_1_50_2_5.partsupp as partsupp, public_experiment_q2_1_50_2_5.supplier as supplier WHERE lineitem.l_suppkey = partsupp.ps_suppkey AND partsupp.ps_suppkey = supplier.s_suppkey";
+	private static final String q3_1 = "SELECT * FROM public_experiment_q3_1_10_2_5.lineitem as lineitem, public_experiment_q3_1_10_2_5.orders as orders, public_experiment_q3_1_10_2_5.customer as customer, public_experiment_q3_1_10_2_5.partsupp as partsupp WHERE lineitem.l_orderkey = orders.o_orderkey AND orders.o_custkey = customer.c_custkey AND lineitem.l_suppkey = partsupp.ps_suppkey";
+	private static final String q3_2 = "SELECT * FROM public_experiment_q3_1_30_2_5.lineitem as lineitem, public_experiment_q3_1_30_2_5.orders as orders, public_experiment_q3_1_30_2_5.customer as customer, public_experiment_q3_1_30_2_5.partsupp as partsupp WHERE lineitem.l_orderkey = orders.o_orderkey AND orders.o_custkey = customer.c_custkey AND lineitem.l_suppkey = partsupp.ps_suppkey AND partsupp.ps_availqty = 6700 AND orders.o_orderpriority = '1-URGENT'";
+	private static final String q3_3 = "SELECT * FROM public_experiment_q3_1_50_2_5.lineitem as lineitem, public_experiment_q3_1_50_2_5.orders as orders, public_experiment_q3_1_50_2_5.customer as customer, public_experiment_q3_1_50_2_5.partsupp as partsupp WHERE lineitem.l_orderkey = orders.o_orderkey AND orders.o_custkey = customer.c_custkey AND lineitem.l_suppkey = partsupp.ps_suppkey AND partsupp.ps_availqty = 6700 AND orders.o_orderpriority = '1-URGENT'";
 	
 	public static void main(String args[]) throws SQLException, JSONException, IOException{	
 		
 		auth();	
-		SQLHandler database = new SQLHandler(username, password, "disstester"); //initializing our databse object
-		ResultSet rs = database.query("SELECT * FROM main;");
-		Set<String> databaseInstance = database.getQueryResults(rs);
+		SQLHandler database = new SQLHandler(username, password, "traffic_crashes_chicago"); //initializing our database object
+		ResultSet rs = database.query(q_tcc);
+		//SELECT * FROM crashes WHERE roadway_surface_cond='DRY'
+		ArrayList<String> databaseFirstInstance = database.getQueryResultsTCC(rs);
 		
+//		analyzeDB(databaseFirstInstance);
+
+		ArrayList<String> violating = new ArrayList<String>();
+		ArrayList<String> nonViolating = new ArrayList<String>();
+		Set<String> dups = new HashSet<String>(getDuplicateKeys(databaseFirstInstance));
+		
+		for (String a : databaseFirstInstance) {
+			String primaryKey = a.split(",")[0];
+			if(dups.contains(primaryKey)) {
+				violating.add(a);
+			}
+			else {
+				nonViolating.add(a);
+			}
+		}
+//		analyzeDB(violating);
+		
+		
+		double lamda = 0.75;
+		double epsilon = 0.1;
+		double n = (1/(2*Math.pow(epsilon, 2.0))) * Math.log(2/lamda);
+		
+		//original_CQA((int)n, databaseFirstInstance);
+		optimised_CQA((int)n, violating, nonViolating);
+
+	}
+	
+	public static void original_CQA(int n, ArrayList<String> db) {
 		ArrayList<String> constraintViolating = new ArrayList<String>();
 		String tupleToRemove = null;
 		Random rand = new Random();
+		ArrayList<String> currentQueryResults = new ArrayList<String>();
 		Map<String, Integer> results = new HashMap<String, Integer>();
+
+		long timer = System.currentTimeMillis();
 		
-		double lamda = 0.01;
-		double epsilon = 0.01;
-		double n = (1/(2*Math.pow(epsilon, 2.0))) * Math.log(2/lamda);
-		
-		for(int i=0; i<n; i++) {
-			Set<String> currentInstance = new HashSet<String>(databaseInstance);
-			
+		for(int i=1; i<=n; i++) {
+			ArrayList<String> currentInstance = new ArrayList<String>(db);
+			long iter = System.currentTimeMillis();
+
 			//while the instance is inconsistent
 			while(isInconsistent(currentInstance)) {
-				constraintViolating = violatingConstraintTuples(currentInstance); //get an ArrayList<String> of all inconsistent tuples
+
+				constraintViolating = violatingConstraintTuples(currentInstance); //get an ArrayList<String> of all inconsistent tuples	
 				
 				tupleToRemove = constraintViolating.get(rand.nextInt(constraintViolating.size())); //choose one of the violations with prop 1/constraintViolating.length
 
 				currentInstance.remove(tupleToRemove); //update databaseInstance
+
+			}
+
+			currentQueryResults = queryTCC(currentInstance);
+			results = updateResultMap(currentQueryResults, results);
+
+			System.out.println("Iteration " + i + "/" + (int)n + " | Done in " + (System.currentTimeMillis()-iter)/1000 + "s");
+		}
+		//printResults(results, n-1);
+		System.out.println((System.currentTimeMillis()-timer)/1000);
+	}
+	
+	public static void optimised_CQA(int n, ArrayList<String> violating, ArrayList<String> nonViolating) {
+		String tupleToRemove = null;
+		Random rand = new Random();
+		ArrayList<String> currentQueryResults = new ArrayList<String>();
+		Map<String, Integer> results = new HashMap<String, Integer>();
+		
+		long timer = System.currentTimeMillis();
+		
+		for(int i=0; i<n; i++) {
+			ArrayList<String> currentTotalInstance = new ArrayList<String>(nonViolating);
+			ArrayList<String> currentInstanceOfViolations = new ArrayList<String>(violating);
+			
+			long iter = System.currentTimeMillis();
+			
+			//while the instance is inconsistent
+			while(isInconsistent(currentInstanceOfViolations)) {
+				
+				tupleToRemove = currentInstanceOfViolations.get(rand.nextInt(currentInstanceOfViolations.size())); //choose one of the violations with prop 1/constraintViolating.length
+
+				currentInstanceOfViolations.remove(tupleToRemove); //update databaseInstance
+								
+				String tup = nonViolatingConstraintTuple(currentInstanceOfViolations); //add non violating tuple if constraint has been totally removed
+				if(!tup.equals("")) { currentTotalInstance.add(tup); }
+				
+				currentInstanceOfViolations = violatingConstraintTuples(currentInstanceOfViolations); //update constraint violations
+
 			}
 			
-			results = updateResultMap(currentInstance, results);
-		
+			currentQueryResults = queryTCC(currentTotalInstance);
+			results = updateResultMap(currentQueryResults, results);
+
+			System.out.println("Iteration " + i + "/" + (int)n + " | Done in " + (System.currentTimeMillis()-iter)/1000 + "s");
 		}
-		
-		printResults(results, n);
+		printResults(results, n-1);
+		System.out.println((System.currentTimeMillis()-timer)/1000);
 	}
 
 	public static void auth() throws JSONException, IOException {
@@ -62,50 +143,78 @@ public class CQAalgorithm {
         
 	}
 
-	public static ArrayList<String> violatingConstraintTuples(Set<String> db){
+	public static String nonViolatingConstraintTuple(ArrayList<String> subDB) {
+		Set<String> duplicates = new HashSet<String>(getDuplicateKeys(subDB));
+		
+		for(String each : subDB) {
+			if(!duplicates.contains(each.split(",")[0])) {
+				return each;
+			}
+		}
+		
+		return "";
+	}
+	
+	public static Set<String> getDuplicateKeys(ArrayList<String> db){
+		Map<String, Integer> entries = new HashMap<String, Integer>();
+		Set<String> duplicatePrimaryKeys = new HashSet<String>();
+		String primaryKey;
+		
+		for(String each : db) {
+			primaryKey = each.split(",")[0];
+			if(entries.containsKey(primaryKey)) {
+				entries.put(primaryKey, entries.get(primaryKey)+1);
+			}
+			else {
+				entries.put(primaryKey, 1);
+			}
+		}
+		
+		for(String k : entries.keySet()) {
+			if(entries.get(k)>1) {
+				duplicatePrimaryKeys.add(k);
+			}
+		}
+		
+		return duplicatePrimaryKeys;
+	}
+	
+	//takes a database and returns all rows which violate the primary key constraint
+	public static ArrayList<String> violatingConstraintTuples(ArrayList<String> db){
 
 		ArrayList<String> violations = new ArrayList<String>();
-		Map<String, Integer> keys = new HashMap<String, Integer>();
+		Set<String> duplicateKeys = new HashSet<String>(getDuplicateKeys(db));
 		String primaryKey;
 		
 		for(String tuple : db) {
 			primaryKey = tuple.split(",")[0];
-			if(keys.containsKey(primaryKey)) {
-				int count = keys.get(primaryKey);
-				count++;
-				keys.put(primaryKey, count);
-			}
-			else {
-				keys.put(primaryKey, 1);
-			}
-		}
-		
-		for(String tuple : db) {
-			primaryKey = tuple.split(",")[0];
-			if(keys.get(primaryKey)>1) {
+			if(duplicateKeys.contains(primaryKey)) {
 				violations.add(tuple);
 			}
 		}
 		
 		return violations;
+			
 	}
 	
-	public static Boolean isInconsistent(Set<String> db) {
-		ArrayList<String> keys = new ArrayList<String>();
+	//checks if the primary key is duplicated inside the given database
+	public static Boolean isInconsistent(ArrayList<String> db) {
+		Set<String> keys = new HashSet<String>(); //using a set to reduce search time to O(1) - thus runtime of this method is O(n)
 		
 		for(String each : db) {
-			if(keys.contains(each.split(",")[0])) {
+			String primaryKey = each.split(",")[0];
+			if(keys.contains(primaryKey)) {
 				return true;
 			}
 			else {
-				keys.add(each.split(",")[0]);
+				keys.add(primaryKey);
 			}
 		}
 		
 		return false;
 	}
 	
-	public static Map<String, Integer> updateResultMap(Set<String> db, Map<String, Integer> res){
+	public static Map<String, Integer> updateResultMap(ArrayList<String> db, Map<String, Integer> res){
 		int temp;
 		Map<String, Integer> returnMap = new HashMap<String, Integer>(res);
 		
@@ -124,7 +233,89 @@ public class CQAalgorithm {
 
 	public static void printResults(Map<String, Integer> toPrint, double iterations) {
 		for(String item : toPrint.keySet()) {
-			System.out.println("(" + item + ") - " +  String.format("%.3f", toPrint.get(item)/iterations));
+				System.out.println("(" + item + ") - " +  String.format("%.3f", toPrint.get(item)/iterations));
 		}
+	}
+
+	public static void analyzeDB(ArrayList<String> db) {
+		System.out.println("Total rows: " + db.size());
+		
+		Set<String> test = new HashSet<String>();
+		for(String s : db) {
+			String key = s.split(",")[0];
+			test.add(key);
+		}
+		System.out.println("Total unique keys: " + test.size());
+		
+		Map<String, Integer> groups = new HashMap<String, Integer>();
+		for(String s : db) {
+			String key = s.split(",")[0];
+			if(groups.containsKey(key)) {
+				groups.put(key, groups.get(key)+1);
+			}
+			else {
+				groups.put(key, 1);
+			}
+		}
+		
+		int single = 0;
+		int multiple = 0;
+		int groupedCount = 0;
+		for(String item : groups.keySet()) {
+			if(groups.get(item)==1) {
+				single++;
+			}
+			else {
+				multiple++;
+				groupedCount+=groups.get(item);
+			}
+		}
+
+		System.out.println("Total keys with group>1: " + multiple);
+		System.out.println("Total keys with group=1: " + single);
+		System.out.println("Average group size: " + Double.valueOf(groupedCount/multiple));
+		
+	}
+
+	public static ArrayList<String> queryTCC(ArrayList<String> db){
+		ArrayList<String> ret = new ArrayList<String>();
+		
+		for(String entry : db) {
+			String q = entry.split(",")[10].trim();
+			if(q.equals("DRY")) {
+				ret.add(entry);
+			}
+		}
+		
+		return ret;
+	}
+	
+	public static ArrayList<String> query1(ArrayList<String> db){
+		ArrayList<String> ret = new ArrayList<String>();
+		
+		for(String entry : db) {
+			String ps_availqty = entry.split(",")[15].trim();
+			String l_tax = entry.split(",")[6].trim();
+			if(ps_availqty.equals("674") && l_tax.equals("0.000")) {
+				ret.add(entry);
+			}
+		}
+		
+		return ret;
+	}
+	
+	public static ArrayList<String> query2(ArrayList<String> db){
+		ArrayList<String> ret = new ArrayList<String>();
+		// supplier.s_phone = '26-762-352-2798' AND lineitem.l_shipinstruct = 'NONE'
+		
+		for(String entry : db) {
+			String s_phone = entry.split(",")[22].trim();
+			String l_shipinstruct = entry.split(",")[12].trim();
+			if(s_phone.equals("'26-762-352-2798'") && l_shipinstruct.equals("'NONE'")) {
+				ret.add(entry);
+			}
+		}
+		
+		return ret;
 	}
 }
--- Dissertation/src/SQLHandler.java
+++ Dissertation/src/SQLHandler.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import javafx.util.Pair;
 //sudo pkill -u postgres
 public class SQLHandler {
 	
@@ -56,15 +55,6 @@ public class SQLHandler {
 		
 	}
 	
-	private static void initializeDB() throws SQLException {
-		conn.createStatement().executeUpdate("INSERT INTO main VALUES ('a', 'b')");
-		conn.createStatement().executeUpdate("INSERT INTO main VALUES ('a', 'c')");
-		conn.createStatement().executeUpdate("INSERT INTO main VALUES ('a', 'd')");
-		conn.createStatement().executeUpdate("INSERT INTO main VALUES ('b', 'a')");
-		conn.createStatement().executeUpdate("INSERT INTO main VALUES ('b', 'd')");
-		conn.createStatement().executeUpdate("INSERT INTO main VALUES ('c', 'a')");
-	}
-	
 	public void clearDB() throws SQLException {
 		conn.createStatement().executeUpdate("DELETE FROM main");
 	}
@@ -92,7 +82,7 @@ public class SQLHandler {
 		}
 	}
 	
-	public Set<String> getQueryResults(ResultSet res) throws SQLException {
+	public Set<String> getQueryResultsTest(ResultSet res) throws SQLException {
 		
 		Set<String> queryResults = new HashSet<String>();
 		
@@ -106,4 +96,120 @@ public class SQLHandler {
 		return queryResults;
 	}
 
+	public ArrayList<String> getQueryResults1(ResultSet res) throws SQLException {
+			
+			ArrayList<String> queryResults = new ArrayList<String>();
+			String[] resultList = new String[17];
+			String[] key = new String[4];
+ 			
+			while (res.next()) {
+	            key[0] = res.getString("l_orderkey");
+	            key[1] = res.getString("ps_partkey");
+	            key[2] = res.getString("ps_suppkey");
+	            key[3] = res.getString("l_linenumber");
+	            
+	            resultList[0] = res.getString("l_partkey");
+	            resultList[1] = res.getString("l_suppkey");
+	            resultList[2] = res.getString("l_quantity");
+	            resultList[3] = res.getString("l_extendedprice");
+	            resultList[4] = res.getString("l_discount");
+	            resultList[5] = res.getString("l_tax");
+	            resultList[6] = res.getString("l_returnflag");
+	            resultList[7] = res.getString("l_linestatus");
+	            resultList[8] = res.getString("l_shipdate");
+	            resultList[9] = res.getString("l_commitdate");
+	            resultList[10] = res.getString("l_receiptdate");
+	            resultList[11] = res.getString("l_shipinstruct");
+	            resultList[12] = res.getString("l_shipmode");
+	            resultList[13] = res.getString("l_comment");
+	            resultList[14] = res.getString("ps_availqty");
+	            resultList[15] = res.getString("ps_supplycost");
+	            resultList[16] = res.getString("ps_comment");
+	            
+	            queryResults.add(String.join("/", key) + "," + String.join(",", resultList));
+			}
+			
+			return queryResults;
+		}
+
+	public ArrayList<String> getQueryResults2(ResultSet res) throws SQLException {
+		
+		ArrayList<String> queryResults = new ArrayList<String>();
+		String[] resultList = new String[23];
+		String[] key = new String[5];
+			
+		while (res.next()) {
+            key[0] = res.getString("l_orderkey");
+            key[1] = res.getString("ps_partkey");
+            key[2] = res.getString("ps_suppkey");
+            key[3] = res.getString("l_linenumber");
+            key[4] = res.getString("s_suppkey");
+            
+            resultList[0] = res.getString("l_partkey");
+            resultList[1] = res.getString("l_suppkey");
+            resultList[2] = res.getString("l_quantity");
+            resultList[3] = res.getString("l_extendedprice");
+            resultList[4] = res.getString("l_discount");
+            resultList[5] = res.getString("l_tax");
+            resultList[6] = res.getString("l_returnflag");
+            resultList[7] = res.getString("l_linestatus");
+            resultList[8] = res.getString("l_shipdate");
+            resultList[9] = res.getString("l_commitdate");
+            resultList[10] = res.getString("l_receiptdate");
+            resultList[11] = res.getString("l_shipinstruct");
+            resultList[12] = res.getString("l_shipmode");
+            resultList[13] = res.getString("l_comment");
+            resultList[14] = res.getString("ps_availqty");
+            resultList[15] = res.getString("ps_supplycost");
+            resultList[16] = res.getString("ps_comment"); 
+            resultList[17] = res.getString("s_name");
+            resultList[18] = res.getString("s_address");
+            resultList[19] = res.getString("l_shipmode");
+            resultList[20] = res.getString("s_nationkey");
+            resultList[21] = res.getString("s_phone");
+            resultList[22] = res.getString("s_acctbal");
+            resultList[23] = res.getString("s_comment");
+            
+            queryResults.add(String.join("/", key) + "," + String.join(",", resultList));
+		}
+		
+		return queryResults;
+	}
+	
+	public ArrayList<String> getQueryResultsTCC(ResultSet res) throws SQLException {
+		
+		ArrayList<String> queryResults = new ArrayList<String>();
+		String[] resultList = new String[20];
+		String[] key = new String[4];
+		
+		while (res.next()) {
+            key[0] = res.getString("crash_date");
+            key[1] = res.getString("street_name");
+            key[2] = res.getString("street_no");
+            key[3] = res.getString("street_direction");
+            resultList[0] = res.getString("posted_speed_limit");
+            resultList[1] = res.getString("traffic_control_device");
+            resultList[2] = res.getString("device_condition");
+            resultList[3] = res.getString("weather_condition");
+            resultList[4] = res.getString("lighting_condition");
+            resultList[5] = res.getString("first_crash_type");
+            resultList[6] = res.getString("trafficway_type");
+            resultList[7] = res.getString("lane_cnt");
+            resultList[8] = res.getString("alignment");
+            resultList[9] = res.getString("roadway_surface_cond");
+            resultList[10] = res.getString("road_defect");
+            resultList[11] = res.getString("crash_type");
+            resultList[12] = res.getString("prim_contributory_cause");
+            resultList[13] = res.getString("sec_contributory_cause");
+            resultList[14] = res.getString("injuries_fatal");
+            resultList[15] = res.getString("injuries_incapacitating");
+            resultList[16] = res.getString("injuries_non_incapacitating");
+            resultList[17] = res.getString("injuries_reported_not_evident");
+            resultList[18] = res.getString("injuries_no_indication");
+            resultList[19] = res.getString("injuries_unknown");
+  
+            queryResults.add(String.join("/", key) + "," + String.join(",", resultList));
+		}
+		return queryResults;
+	}
 }
